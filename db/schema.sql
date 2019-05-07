b3a8d6f7729cfd1bf9cf4b038be63c1b84ece45b
Erik Johnson <ej3684@yahoo.com>
2019-05-07 15:57:33 -0400

    added seeds

--- db/schema.sql
+++ db/schema.sql
@@ -11,9 +11,3 @@ CREATE TABLE burgers
 	PRIMARY KEY (id)
 ); 
 
-INSERT INTO burgers (burger_name) VALUES ('Crescent Burger');
-INSERT INTO burgers (burger_name) VALUES ('Luther Burger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Burger Turbo');
-INSERT INTO burgers (burger_name) VALUES ('Elvis Burger');
-INSERT INTO burgers (burger_name) VALUES ('Turducken Slammer Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Rinacita Burger', true);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,4 @@
-CREATE DATABASE burgers_db;
 
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
-); 
 
 INSERT INTO burgers (burger_name) VALUES ('Crescent Burger');
 INSERT INTO burgers (burger_name) VALUES ('Luther Burger');
