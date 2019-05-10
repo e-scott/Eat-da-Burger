cbb290c6d7a933dbc26f881fcc3849cdc5b4aeb8
Erik Johnson <ej3684@yahoo.com>
2019-05-10 10:59:23 -0400

    v14

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,19 @@ var mysql = require('mysql');
 var connection;
 
 if(process.env.JAWSDB_URL) {
+    console.log('jaws1');
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+    console.log('jaws2');
 } else {
+    console.log('jaws3')
   connection = mysql.createConnection({
+    port: 3306,
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'burgers_db'
   });
-};
+}
 
 connection.connect(function(err) {
     if (err) {
--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ var connection = require('../config/connection.js');
 function printQuestionMarks(num){
   var arr = [];
 
-  for (var i=0; i<num; i++){
+  for (var i = 0; i < num; i++){
     arr.push('?')
   }
 
